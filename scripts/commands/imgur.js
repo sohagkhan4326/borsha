0e7c70c07b2a556cedd4d61c470e272626f2b251
sabbir432s <162293878+sabbir432s@users.noreply.github.com>
2024-03-06 01:41:27 +0600

    Update imgur.js

--- scripts/commands/imgur.js
+++ scripts/commands/imgur.js
@@ -1,28 +1,40 @@
-const axios = require('axios');
-
 module.exports.config = {
-  name: "imgur2",
-  version: "2.0.0",
+  name: "imgur",
+  version: "1.0.0",
   permission: 0,
   credits: "Nayan",
-  description: "convert image/video into Imgur link",
-  prefix: false,
-  category: "tools",
-  usages: "reply [image, video]",
+  description: "",
+  prefix: true, 
+  category: "user", 
+  usages: "Link",
   cooldowns: 5,
+  dependencies: {
+    "imgur-upload-api": ''
+  }
 };
 
-module.exports.run = async function ({ api, event }) {
-    const dip = event.messageReply?.attachments[0]?.url;
-    if (!dip) {
-      return api.sendMessage('Please reply to an image or video.', event.threadID, event.messageID);
+module.exports.run = async function({ api, event, args }) {
+  const linkanh = event.messageReply.attachments[0].url || args.join(" ");
+    const axios = require("axios")
+    const request = require("request")
+    const fs = require("fs-extra")
+  var imgur = require('imgur-upload-api'),
+  path = require('path');
+  const myClientID = 'Client-ID 3fb071726880bbb'
+  imgur.setClientID(myClientID);
+
+  imgur.upload(linkanh, function (err,res) {
+    console.log(res)
+    const link = res.data.link;
+    const type = res.data.type;
+    var msg = [];
+    {
+        msg += `TYPE: ${type}\nLINK: ${link}`
     }
-    try {
-      const res = await axios.get(`https://all-image-genator-d1p.onrender.com/dipto/imgur?url=${encodeURIComponent(dip)}`);
-      const dipto = res.data.data;
-         api.sendMessage(dipto, event.threadID, event.messageID);
-    } catch (error) {
-      console.error(error);
-      return api.sendMessage('Failed to convert image or video into link.', event.threadID, event.messageID);
-    }
-};
+    return api.sendMessage({
+        body: msg
+
+    }, event.threadID, event.messageID);
+  });
+
+}
