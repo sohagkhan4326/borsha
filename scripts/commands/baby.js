95cc4768b29652e6db0ef61f7a88f97a814bf9bf
sabbir432s <162293878+sabbir432s@users.noreply.github.com>
2024-03-07 00:18:32 +0600

    Update baby.js

--- scripts/commands/baby.js
+++ scripts/commands/baby.js
@@ -27,7 +27,7 @@ return api.sendMessage(r,event.threadID,event.messageID);
 //-------------------------------------------//
   else if (args[0] === 'remove') {
   const fina = dipto.replace("remove ", "");
-        const respons = await axios.get(`https://yw2tgw-3000.csb.app/dipto?remove=${fina}`);
+        const respons = await axios.get(`https://noobs-apihouse.onrender.com/dipto?remove=${fina}`);
         const dat = respons.data.message;
         api.sendMessage(`${dat}`, event.threadID, event.messageID);
     }
@@ -36,20 +36,20 @@ else if (args[0] === 'rm' && dipto.includes('-')) {
       const fina = dipto.replace("rm ", "");
      const fi = fina.split(' - ')[0]
      const f = fina.split(' - ')[1]
-        const respons = await axios.get(`https://yw2tgw-3000.csb.app/dipto?remove=${fi}&index=${f}`);
+        const respons = await axios.get(`https://noobs-apihouse.onrender.com/dipto?remove=${fi}&index=${f}`);
         const da = respons.data.message;
         api.sendMessage(`${da}`, event.threadID, event.messageID);
 }
     //-------------------------------------//
    else if (args[0] === 'list') {
-        const respo = await axios.get(`https://yw2tgw-3000.csb.app/dipto?list=all`);
+        const respo = await axios.get(`https://noobs-apihouse.onrender.com/dipto?list=all`);
         const d = respo.data.length;
         api.sendMessage(`Total Teach ${d}`, event.threadID, event.messageID);
     }
     //-------------------------------------//
       else if (args[0] === 'msg' || args[0] === 'message') {
   const fuk = dipto.replace("msg ", "");
-        const respo = await axios.get(`https://yw2tgw-3000.csb.app/dipto?list=${fuk}`);
+        const respo = await axios.get(`https://noobs-apihouse.onrender.com/dipto?list=${fuk}`);
         const d = respo.data.data;
         api.sendMessage(`Message ${fuk} = ${d}`, event.threadID, event.messageID);
       }
@@ -59,7 +59,7 @@ else if (args[0] === 'rm' && dipto.includes('-')) {
         if (command.length < 2) {
             return api.sendMessage('❌ | Invalid format! Use edit [YourMessage] - [NewReply]', event.threadID, event.messageID);
         }
-        const res = await axios.get(`https://yw2tgw-3000.csb.app/dipto?edit=${args[1]}&replace=${command}`);
+        const res = await axios.get(`https://noobs-apihouse.onrender.com/dipto?edit=${args[1]}&replace=${command}`);
         const dA = res.data.message;
         api.sendMessage(`changed ${dA}`, event.threadID, event.messageID);
     } 
@@ -72,7 +72,7 @@ else if (args[0] === 'rm' && dipto.includes('-')) {
             if (command.length < 2) {
             return api.sendMessage('❌ | Invalid format! Use [YourMessage] - [Reply1], [Reply2], [Reply3]... OR remove [YourMessage] OR list OR edit [YourMessage] - [NewReply]', event.threadID, event.messageID);
         }
-        const re = await axios.get(`https://yw2tgw-3000.csb.app/dipto?teach=${final}&reply=${command}`);
+        const re = await axios.get(`https://noobs-apihouse.onrender.com/dipto?teach=${final}&reply=${command}`);
         const tex = re.data.message;
         api.sendMessage(`✅ Replies added ${tex}`, event.threadID, event.messageID);
     }
@@ -84,13 +84,13 @@ else if (args[0] === 'teach' && args[1] === 'amar'){
         if (command.length < 2) {
             return api.sendMessage('❌ | Invalid format! Use [YourMessage] - [Reply1], [Reply2], [Reply3]... OR remove [YourMessage] OR list OR edit [YourMessage] - [NewReply]', event.threadID, event.messageID);
         }
-        const re = await axios.get(`https://yw2tgw-3000.csb.app/ipto?teach=${final}&senderID=${uid}&reply=${command}`);
+        const re = await axios.get(`https://noobs-apihouse.onrender.com/dipto?teach=${final}&senderID=${uid}&reply=${command}`);
         const tex = re.data.message;
         api.sendMessage(`✅ Replies added ${tex}`, event.threadID, event.messageID);
     }
  //-------------------------------------//
     else if (dipto.includes('amar name ki') || dipto.includes('amr nam ki') || dipto.includes('amar nam ki') || dipto.includes('amr name ki')){
-    const response = await axios.get(`https://yw2tgw-3000.csb.app/dipto?text=amar name ki&senderID=${uid}`);
+    const response = await axios.get(`https://noobs-apihouse.onrender.com/dipto?text=amar name ki&senderID=${uid}`);
     const data = response.data.reply;
     api.sendMessage(`${data}`, event.threadID, event.messageID);
        }
