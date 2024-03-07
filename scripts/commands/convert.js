6a3d1030e2a91686d8370de2dabd68efac2bed88
sabbir432s <162293878+sabbir432s@users.noreply.github.com>
2024-03-07 20:26:00 +0600

    Update convert.js

--- scripts/commands/convert.js
+++ scripts/commands/convert.js
@@ -3,7 +3,7 @@ module.exports.config = {
     version: "1.0.0",
     permission: 0,
     credits: "Nayan",
-    prefix: true,
+    prefix: false,
     description: "",
     category: "user",
     usages: "",
@@ -19,7 +19,7 @@ var audioss = []
     var { data } = await axios.get(audio ,{  method: 'GET',  responseType: 'arraybuffer' });
                 fs.writeFileSync(__dirname + "/cache/vdtoau.m4a", Buffer.from(data, 'utf-8'));
   audioss.push(fs.createReadStream(__dirname + "/cache/vdtoau.m4a"));
-    var msg = { body : "𝗖𝗼𝘃𝗲𝗿𝘁 𝘀𝗮𝗻𝗴 𝗠𝗣𝟯  ️🎶\n𝐌𝐨𝐡𝐚𝐦𝐦𝐚𝐝 𝐧𝐚𝐲𝐚𝐧", attachment: audioss}
+    var msg = { body : "𝗖𝗼𝘃𝗲𝗿𝘁 𝘀𝗮𝗻𝗴 𝗠𝗣𝟯  ️🎶\n[𝐎𝐖𝐍𝐄𝐑:☞𝐒𝐎𝐇𝐀𝐆 𝐊𝐇𝐀𝐍 (✷‿✷)☜", attachment: audioss}
   api.sendMessage(msg, event.threadID, event.messageID)
 } catch(e){
     console.log(e)
