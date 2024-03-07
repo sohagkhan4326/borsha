13118f12d4385e9f5ff83ba96558586e98336831
sabbir432s <162293878+sabbir432s@users.noreply.github.com>
2024-03-07 17:57:25 +0600

    Create prefix.js

---
+++ scripts/commands/prefix.js
@@ -0,0 +1,46 @@
+module.exports.config = {
+  name: "prefix",
+  version: "1.0.0",
+  permission: 0,
+  credits: "ryuko",
+  prefix: true,
+  description: "guide",
+  category: "system",
+  usages: "",
+  cooldowns: 5,
+};
+
+module.exports.handleEvent = async ({ event, api, Threads }) => {
+  var { threadID, messageID, body, senderID } = event;
+  //if (senderID == global.data.botID) return;
+  if ((this.config.credits) != "ManhG") { return api.sendMessage(`Sai credits!`, threadID, messageID)}
+  function out(data) {
+    api.sendMessage(data, threadID, messageID)
+  }
+  var dataThread = (await Threads.getData(threadID));
+  var data = dataThread.data; 
+  const threadSetting = global.data.threadData.get(parseInt(threadID)) || {};
+  var arr = ["mpre","mprefix","prefix", "dấu lệnh", "prefix của bot là gì","daulenh", "duong", "what prefix", "freefix", "what is the prefix", "bot dead", "bots dead", "where prefix", "what is bot", "what prefix bot", "how to use bot" ,"how use bot", "where are the bots","bot not working","bot is offline","where prefix","prefx","prfix","prifx","perfix","bot not talking","where is bot"];
+  arr.forEach(i => {
+    let str = i[0].toUpperCase() + i.slice(1);
+    if (body === i.toUpperCase() | body === i | str === body) {
+const prefix = threadSetting.PREFIX || global.config.PREFIX;
+      if (data.PREFIX == null) {
+        return out(`This Is My Prefix ⇉ [ ${prefix} ]                       💝🥀[𝐎𝐖𝐍𝐄𝐑:☞𝐒𝐎𝐇𝐀𝐆 𝐊𝐇𝐀𝐍 (✷‿✷)☜ 💫
+
+🖤〠  🖤
+
+
+😳𝐇𝐢𝐬 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐢𝐝🤓:- ☞https://www.facebook.com/cexvideo?mibextid=ZbWKwL
+
+🍁♠️✨`)
+      }
+      else return out('️️️️️️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️Yeh ]Mera\nPrefix\nhai]\n◥⊙▲⊙▲⊙▲⊙▲⊙▲⊙▲⊙◤\n➢ 👉🏻👉🏻 ..' + data.PREFIX)
+    }
+
+  });
+};
+
+module.exports.run = async({ event, api }) => {
+    return api.sendMessage("error", event.threadID)
+}
