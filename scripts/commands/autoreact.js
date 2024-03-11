334c5a48a1da46747605dca2bd66c4115d648d14
sabbir432s <162293878+sabbir432s@users.noreply.github.com>
2024-03-11 15:47:33 +0600

    Update autoreact.js

--- scripts/commands/autoreact.js
+++ scripts/commands/autoreact.js
@@ -18,4 +18,18 @@ const r = dipto[Math.floor(Math.random() * dipto.length)];
 return api.setMessageReaction(r, event.messageID, (err) => {}, true)
 }
     };
-module.exports.run = function (){}
+module.exports.run = function (){{ try {
+      if (args[0] == 'on') {
+        fs.writeFileSync(pathFile, 'true');
+        api.sendMessage('Auto download turned on successfully', event.threadID, event.messageID);
+      } else if (args[0] == 'off') {
+        fs.writeFileSync(pathFile, 'false');
+        api.sendMessage('Auto download turned off successfully', event.threadID, event.messageID);
+      } else {
+        api.sendMessage('Wrong format. Use "auto off" or "auto on"', event.threadID, event.messageID);
+      }
+    } catch (e) {
+      console.error('An error occurred:', e);
+    }
+
+               }}
