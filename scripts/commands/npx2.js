84d0bbd696dad506aedb6d7582d65a4ff0f8ae8d
sohagkhan4326 <161201084+sohagkhan4326@users.noreply.github.com>
2024-03-23 15:38:47 +0600

    Create npx2.js

---
+++ scripts/commands/npx2.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+module.exports.config = {
+	name: "2npxs",
+    version: "1.0.1",
+	hasPermssion: 0,
+	credits: "VanHung - Fixed by LTD", 
+	description: "hihihihi",
+  prefix: true,
+	category: "no prefix",
+	usages: "mew",
+    cooldowns: 5, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	if (event.body.indexOf("🙀")==0 || event.body.indexOf("🐱")==0 || event.body.indexOf("😽")==0 || event.body.indexOf("🐱")==0) {
+		var msg = {
+				body: "-!mew🐱",
+				attachment: fs.createReadStream(__dirname + `/noprifix/fiha.mp3`)
+			}
+			api.sendMessage( msg, threadID, messageID);
+    api.setMessageReaction("🐱", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+} 
