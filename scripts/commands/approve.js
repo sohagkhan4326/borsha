41454cd22f87e6f364481f8de077db0bc1c4261d
sohagkhan4326 <161201084+sohagkhan4326@users.noreply.github.com>
2024-03-24 07:56:49 +0600

    Update approve.js

--- scripts/commands/approve.js
+++ scripts/commands/approve.js
@@ -1,142 +1,147 @@
 module.exports.config = {
-	name: "approve",
-	version: "1.0.2",
-	hasPermssion: 2,
-	credits: "Md Rajib",
-	description: "approve the gc using bots xD",
+  name: "approve",
+  version: "2.0.0",
+  permission: 0,
+  credits: "nayan",
+  description: "approve thread using thread id",
   prefix: false,
-	category: "Admin",
-    cooldowns: 5
+  category: "admin",
+  usages: "approve [group/remove] [threadid]",
+  cooldowns: 5,
 };
 
-
-const dataPath = __dirname + "/cache/approvedThreads.json";
-const dataPending = __dirname + "/cache/pendingdThreads.json";
-const fs = require("fs");
-
-module.exports.onLoad = () => {
-	if (!fs.existsSync(dataPath)) fs.writeFileSync(dataPath, JSON.stringify([]));
-  if (!fs.existsSync(dataPending)) fs.writeFileSync(dataPending, JSON.stringify([]));
+module.exports.languages = {
+    "vi": {
+        "listAdmin": 'Danh sách toàn bộ người điều hành bot: \n\n%1',
+        "notHavePermssion": 'Bạn không đủ quyền hạn để có thể sử dụng chức năng "%1"',
+        "addedNewAdmin": 'Đã thêm %1 người dùng trở thành người điều hành bot:\n\n%2',
+        "removedAdmin": 'Đã gỡ bỏ %1 người điều hành bot:\n\n%2'
+    },
+    "en": {
+        "listAdmin": 'approved list : \n\n%1',
+        "notHavePermssion": 'you have no permission to use "%1"',
+        "addedNewAdmin": 'approved %1 box :\n\n%2',
+        "removedAdmin": 'remove %1 box in approve lists :\n\n%2'
+    }
 }
-module.exports.handleReply = async function ({ event, api, Currencies, handleReply, Users, args }) {
-    if (handleReply.author != event.senderID) return;
-    const { body, threadID, messageID, senderID } = event;
-    const { type } = handleReply;
-    let data = JSON.parse(fs.readFileSync(dataPath));
-    let dataP = JSON.parse(fs.readFileSync(dataPending));
-    let idBox = (args[0]) ? args[0] : threadID;
-  switch (type) {
-        case "pending": {
-          switch (body) {
-                case `A`: {
-   			data.push(idBox);
-   			fs.writeFileSync(dataPath, JSON.stringify(data, null, 2));
-   			api.sendMessage(`» Successfully approved the box:\n${idBox}`, threadID, () => {
-          dataP.splice(dataP.indexOf(idBox), 1);
-    		fs.writeFileSync(dataPending, JSON.stringify(dataP, null, 2));
-    	}, messageID)
-        }
-        }
+
+module.exports.run = async function ({ api, event, args, Threads, Users, permssion, getText }) {
+    const content = args.slice(1, args.length);
+    const { threadID, messageID, mentions } = event;
+    const { configPath } = global.client;
+    const { APPROVED } = global.config;
+    const { userName } = global.data;
+    const { writeFileSync } = global.nodemodule["fs-extra"];
+    const mention = Object.keys(mentions);
+    delete require.cache[require.resolve(configPath)];
+    var config = require(configPath);
+    
+       
+    switch (args[0]) {
+        case "list":
+        case "all":
+        case "-a": {
+            const listAdmin = APPROVED || config.APPROVED || [];
+            var msg = [];
+
+            for (const idAdmin of listAdmin) {
+                if (parseInt(idAdmin)) {
+                  let boxname;
+                  try {
+        const groupName = await global.data.threadInfo.get(idAdmin).threadName || "name does not exist"
+        boxname = `group name : ${groupName}\ngroup id : ${idAdmin}`;
+      } catch (error) {
+        const userName = await Users.getNameUser(idAdmin);
+        boxname = `user name : ${userName}\nuser id : ${idAdmin}`;
       }
-    }
-  }
-module.exports.run = async ({ event, api, args, Threads, handleReply, Users }) => {
-	const { threadID, messageID, senderID } = event;
-	let data = JSON.parse(fs.readFileSync(dataPath));
-  let dataP = JSON.parse(fs.readFileSync(dataPending));
-  let msg = "";
-  var lydo = args.splice(2).join(" ");
-  let idBox = (args[0]) ? args[0] : threadID;
-        if (args[0] == "list" || args[0] == "l") {
-    	msg = `=====「 GC THAT HAD BEEN APPROVED: ${data.length} 」 ====`;
-    	let count = 0;
-    	for (e of data) {
-        let threadInfo = await api.getThreadInfo(e);
-          let threadName = threadInfo.threadName ? threadInfo.threadName : await Users.getNameUser(e);
-    		msg += `\n〘${count+=1}〙» ${threadName}\n${e}`;
-    	}
-    	api.sendMessage(msg, threadID, (error, info) => {
-        global.client.handleReply.push({
-            name: this.config.name,
-            messageID: info.messageID,
-            author: event.senderID,
-            type: "a",
-        })
-    }, messageID);
+                  msg.push(`\n${boxname}`);
+                }
+            };
+
+            return api.sendMessage(`approved users and groups :\n${msg.join('\n')}`, threadID, messageID);
         }
-     else if (args[0] == "pending" || args[0] == "p") {
-    	msg = `=====「 THREADS NEED TO BE APPROVE: ${dataP.length} 」 ====`;
-    	let count = 0;
-    	for (e of dataP) {
-        let threadInfo = await api.getThreadInfo(e);
-          let threadName = threadInfo.threadName ? threadInfo.threadName : await Users.getNameUser(e);
-    		msg += `\n〘${count+=1}〙» ${threadName}\n${e}`;
-    	}
-    	api.sendMessage(msg, threadID, (error, info) => {
-        global.client.handleReply.push({
-            name: this.config.name,
-            messageID: info.messageID,
-            author: event.senderID,
-            type: "pending",
-        })
-    }, messageID);
-     }
-       else if (args[0] == "help" || args[0] == "h") {
-         const tst = (await Threads.getData(String(event.threadID))).data || {};
-  const pb = (tst.hasOwnProperty("PREFIX")) ? tst.PREFIX : global.config.PREFIX;
-  const nmdl = this.config.name
-  const cre = this.config.credits
-        return api.sendMessage(`=====「 APPROVE 」=====\n\n${pb}${nmdl} l/list => see list of approved boxes\n\n${pb}${nmdl} p/pending => see the list of unapproved boxes\n\n${pb}${nmdl} d/del => with ID to remove from bot used list\n\n${pb}${nmdl} => Attach an ID to browse that box\n\n⇒ ${cre} ⇐`, threadID, messageID);
-       }
-      
-    else if (args[0] == "del" || args[0] == "d") {
-    	idBox = (args[1]) ? args[1] : event.threadID;
-      if (isNaN(parseInt(idBox))) return api.sendMessage("[ ERR ] Not a number", threadID, messageID);
-    	if (!data.includes(idBox)) return api.sendMessage("[ ERR ] Box is not pre-approved!", threadID, messageID);
-      api.sendMessage(`[ OK ] Your group has been removed from the browsing list by the admin for the reason: ${lydo}`, idBox);
-    	api.sendMessage(`[ OK ] Box has been removed from the list of allowed bots`, threadID, () => {
-    		data.splice(data.indexOf(idBox), 1);
-    		fs.writeFileSync(dataPath, JSON.stringify(data, null, 2));
-    	}, messageID)
-    }
-    else if (isNaN(parseInt(idBox))) api.sendMessage("[ ERR ] The ID you entered is not valid", threadID, messageID);
-    else if (data.includes(idBox)) api.sendMessage(`[ - ] ID ${idBox} pre-approved!`, threadID, messageID);
-   	else api.sendMessage("আপনার গ্রুপ এ approved করে দিলাম তো 😇😌\n\n তো আমার রোবোট টি ভালো করে চলান 🌸❤️\nআসা করি আমার বট টি দিয়ে কনো খারাপ কাজ করবেন নাহ \n\n যে কোনো সমস্যা হলে আমার বস সোহাগ এর সাথে কথা বলতে পারেন নিচে তার What’s up বা পাবলিক পোস্ট এর নাম দেওয়া হোলো এটা আপনি যে কোনো জাগায় অনুসন্ধান করলে আমার বস কে পেয়ে জাবেন \n\n [𝐎𝐖𝐍𝐄𝐑:☞𝐒𝐎𝐇𝐀𝐆 𝐊𝐇𝐀𝐍 (✷‿✷)☜", idBox, (error, info) => {
-   		api.changeNickname(` 〖 ${global.config.PREFIX} 〗 ➺ ${(!global.config.BOTNAME) ? "" : global.config.BOTNAME}`, idBox, global.data.botID);
-      const axios = require('axios');
-	const request = require('request');
-	const fs = require("fs");
-   let admID = "100032789873324";    
-  
-      api.getUserInfo(parseInt(admID), (err, data) => {
-      if(err){ return console.log(err)}
-     var obj = Object.keys(data);
-    var firstname = data[obj].name.replace("@", "");  
-      
-      axios.get('https://anime-api.priyanshu91.repl.co/img/anime').then(res => {
-	let ext = res.data.url.substring(res.data.url.lastIndexOf(".") + 1);
-	let callback = function () {
-      api.sendMessage({body: `❒❒ BOT ARE NOW CONNECTED ❒❒\n=====================\n┏━━━━ 🖤 ━━━━┓
-  ༆-✿𝐒𝐎𝐇𝐀𝐆 𝐊𝐇𝐀𝐍༊࿐ 
-┗━━━    🖤 ━━━━┛\n=====================\n➪BOT: ${global.config.BOTNAME}\n➪ Prefix: ${global.config.PREFIX}\n➪ Users: ${global.data.allUserID.length}\n Groups: ${global.data.allThreadID.length}\n=====================\n[]---------------------------------------[]\nUse '${global.config.PREFIX}Help' T0o View The Commands That Available!\n[]---------------------------------------[]\n⌨ Made by: ${firstname}\n`, mentions: [{
-                           tag: firstname,
-                           id: admID,
-                           fromIndex: 0,
-                 }],
-						attachment: fs.createReadStream(__dirname + `/cache/duyet.${ext}`)
-					}, idBox,() => fs.unlinkSync(__dirname + `/cache/duyet.${ext}`));
-				};
-				request(res.data.url).pipe(fs.createWriteStream(__dirname + `/cache/duyet.${ext}`)).on("close", callback);
-			}) 
-      })
-   		if (error) return api.sendMessage("[ ERR ] Something went wrong, make sure the id you entered is valid and the bot is in the box!", threadID, messageID);
-   		else {
-   			data.push(idBox);
-   			fs.writeFileSync(dataPath, JSON.stringify(data, null, 2));
-   			api.sendMessage(`[ OK ] Successfully Approved 😌:\n${idBox}`, threadID, () => {
-          dataP.splice(dataP.indexOf(idBox), 1);
-    		fs.writeFileSync(dataPending, JSON.stringify(dataP, null, 2));
-    	}, messageID)
+
+        case "box": {
+            if (permssion != 3) return api.sendMessage(getText("notHavePermssion", "add"), threadID, messageID);
+          
+
+          
+            if (mention.length != 0 && isNaN(content[0])) {
+              
+                var listAdd = [];
+
+                for (const id of mention) {
+                  
+                    APPROVED.push(id);
+                    config.APPROVED.push(id);
+                    listAdd.push(`${id} - ${event.mentions[id]}`);
+                };
+
+                writeFileSync(configPath, JSON.stringify(config, null, 2), 'utf8');
+                return api.sendMessage(getText("addedNewAdmin", mention.length, listAdd.join("\n").replace(/\@/g, "")), threadID, messageID);
+            }
+            else if (content.length != 0 && !isNaN(content[0])) {
+                APPROVED.push(content[0]);
+                config.APPROVED.push(content[0]);
+                
+                  let boxname;
+                  try {
+        const groupname = await global.data.threadInfo.get(content[0]).threadName || "name does not exist";
+        boxname = `group name : ${groupname}\ngroup id : ${content[0]}`;
+      } catch (error) {
+        const username = await Users.getNameUser(content[0]);
+        boxname = `user name : ${username}\nuser id : ${content[0]}`;
+      }
+                writeFileSync(configPath, JSON.stringify(config, null, 2), 'utf8');
+                return api.sendMessage('this box has been approved', content[0], () => {
+                return api.sendMessage(getText("addedNewAdmin", 1, `${boxname}`), threadID, messageID);
+                });
+            }
+            else return global.utils.throwError(this.config.name, threadID, messageID);
         }
-   	});
-  }
+        
+
+        case "remove":
+        case "rm":
+        case "delete": {
+            if (permssion != 3) return api.sendMessage(getText("notHavePermssion", "delete"), threadID, messageID);
+            if (mentions.length != 0 && isNaN(content[0])) {
+                const mention = Object.keys(mentions);
+                var listAdd = [];
+
+                for (const id of mention) {
+                    const index = config.APPROVED.findIndex(item => item == id);
+                    APPROVED.splice(index, 1);
+                    config.APPROVED.splice(index, 1);
+                    listAdd.push(`${id} - ${event.mentions[id]}`);
+                };
+
+                writeFileSync(configPath, JSON.stringify(config, null, 2), 'utf8');
+                return api.sendMessage(getText("removedAdmin", mention.length, listAdd.join("\n").replace(/\@/g, "")), threadID, messageID);
+            }
+            else if (content.length != 0 && !isNaN(content[0])) {
+                const index = config.APPROVED.findIndex(item => item.toString() == content[0]);
+                APPROVED.splice(index, 1);
+                config.APPROVED.splice(index, 1);
+                
+                  let boxname;
+                  try {
+        const groupname = await global.data.threadInfo.get(content[0]).threadName || "name does not exist";
+        boxname = `group name : ${groupname}\ngroup id : ${content[0]}`;
+      } catch (error) {
+        const username = await Users.getNameUser(content[0]);
+        boxname = `user name : ${username}\nuser id : ${content[0]}`
+      }
+                writeFileSync(configPath, JSON.stringify(config, null, 2), 'utf8');
+                return api.sendMessage('this box has been removed from approved list', content[0], () => {
+                return api.sendMessage(getText("removedAdmin", 1, `${boxname}`), threadID, messageID);
+                });
+            }
+            else global.utils.throwError(this.config.name, threadID, messageID);
+        }
+
+        default: {
+            return global.utils.throwError(this.config.name, threadID, messageID);
+        }
+    };
+}
