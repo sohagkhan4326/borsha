2b66790bc0f04733a87c81da2b30217a5aec6689
sohagkhan4326 <161201084+sohagkhan4326@users.noreply.github.com>
2024-03-25 23:14:42 +0600

    Update chor.js

--- scripts/commands/chor.js
+++ scripts/commands/chor.js
@@ -36,13 +36,13 @@ try {
 	const ctx = canvas.getContext('2d');
 	const background = await Canvas.loadImage('https://i.imgur.com/ES28alv.png');
   
-	var avatar = await request.get(`https://graph.facebook.com/${id}/picture?width=512&height=512&acccess_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`);
+	var avatar = await request.get(`https://graph.facebook.com/${id}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`);
 	avatar = await this.circle(avatar.body);
 	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 	ctx.drawImage(await Canvas.loadImage(avatar), 48, 410, 111, 111);
 	const imageBuffer = canvas.toBuffer();
 	fs.writeFileSync(path_toilet,imageBuffer);
-	 api.sendMessage({attachment: fs.createReadStream(path_toilet, {'highWaterMark': 128 * 1024}), body: "মুরগির ডিম চুরি করতে গিয়া ধরা থাইসে 🐸👻"}, event.threadID, () => fs.unlinkSync(path_toilet), event.messageID);
+	 api.sendMessage({attachment: fs.createReadStream(path_toilet, {'highWaterMark': 128 * 1024}), body: "মুরগীর ডিম  চুরি করতে গিয়া ধরা খাইসে_🐸😹🤛"}, event.threadID, () => fs.unlinkSync(path_toilet), event.messageID);
 }
 catch(e) {api.sendMessage(e.stack, event.threadID )}
-  }
+    }
