0b3ae59aa8e16578f2585700a61cae76e042a659
sohagkhan4326 <161201084+sohagkhan4326@users.noreply.github.com>
2024-03-28 00:07:08 +0600

    Update ramadan.js

--- scripts/commands/ramadan.js
+++ scripts/commands/ramadan.js
@@ -1,67 +1,43 @@
 module.exports.config = {
-  name: "ramadan",
-  version: "0.0.1",
-  permission: 0,
-  prefix: true,
-  credits: "Nayan",
-  description: "Ramadan",
-  category: "user",
-  usages: "",
+    name: "status",
+    version: "1.0.0",
+    permission: 0,
+    credits: "Rahad",
+    description: "Status video",
+    prefix: true, 
+    category: "Video", 
+    usages: "status2",
     cooldowns: 5,
-};
-
-
-module.exports.run = async function({
-  event: e,
-  api: a,
-  args: n
-}) {
-  if (!n[0]) return a.sendMessage("====「 🆁🅰🅼🅰🅳🅰🅽 」====\n━━━━━━━━━━━━━\n𝟙. 𝐒𝐞𝐡𝐫𝐢 𝐓𝐢𝐦𝐞\n𝟚. 𝐈𝐟𝐭𝐚𝐫 𝐓𝐢𝐦𝐞\n𝟛. 𝐈𝐟𝐭𝐚𝐫 𝐃𝐮𝐚\n𝟜. 𝐑𝐮𝐣𝐚𝐫 𝐍𝐢𝐲𝐨𝐭\n𝟝. 𝐑𝐮𝐣𝐚 𝐕𝐚𝐧𝐠𝐚𝐫 𝐊𝐚𝐫𝐨𝐧\n𝟞. 𝐉𝐞𝐠𝐮𝐥𝐚 𝐊𝐨𝐫𝐥𝐞 𝐑𝐮𝐣𝐚 𝐕𝐚𝐧𝐠𝐞 𝐧𝐚\n\n𝐑𝐞𝐩𝐥𝐲 𝐓𝐡𝐢𝐬 𝐌𝐬𝐠 𝐀𝐧𝐝 𝐒𝐞𝐥𝐞𝐜𝐭 𝐍𝐮𝐦𝐛𝐞𝐫", e.threadID, ((a, n) => {
-    global.client.handleReply.push({
-      name: this.config.name,
-      messageID: n.messageID,
-      author: e.senderID,
-      type: "create"
-    })
-  }), e.messageID)
-}, module.exports.handleReply = async ({
-  api: e,
-  event: a,
-  client: n,
-  handleReply: t,
-  Currencies: s,
-  Users: i,
-  Threads: o
-}) => {
-  var { p, h } = linkanh();
-  const request = require("request");
-  if ("create" === t.type) {
-    const n = (await p.get(h)).data.data;
-    const time = (await p.get(h)).data.times.time;
-    const msg = (await p.get(h)).data.msg;
-
-
-    return e.sendMessage({
-      body: `${msg}: ${time}`
-
-    }, a.threadID, a.messageID)
-  }
-
-    function linkanh() {
-        const p = require("axios");
-        const n = "http://game2.jagoanvps.cloud:5059";
-        if ("1" == a.body)
-            var h = `${n}/nayan/sehri`;
-        else if ("2" == a.body)
-         var   h = `${n}/nayan/iftar`;
-      else if ("3" == a.body)
-         var   h = `${n}/nayan/iftardua`;
-      else if ("4" == a.body)
-         var   h = `${n}/nayan/rujarniyot`;
-      else if ("5" == a.body)
-         var   h = `${n}/nayan/rujavk`;
-      else if ("6" == a.body)
-         var   h = `${n}/nayan/rujavkn`;
-        return { p, h };
+    dependencies: {
+        "axios": ""
     }
 };
+
+module.exports.run = async function({ api, event, args }) {
+    const axios = require("axios")
+    const request = require("request")
+    const fs = require("fs-extra")
+    const res = await axios.get(`http://nl2-4.deploy.sbs:2016/auto`);
+    var data = res.data.data;
+    var msg = [];
+    let img1 = `${res.data.url.url}`;
+    let cp = `${res.data.url.title}`
+    let rahad = `${res.data.author}`
+
+    let imgs1 = (await axios.get(`${img1}`, {
+        responseType: 'arraybuffer'
+    })).data;
+    fs.writeFileSync(__dirname + "/cache/img1.mp4", Buffer.from(imgs1, "utf-8"));
+    var allimage = [];
+    allimage.push(fs.createReadStream(__dirname + "/cache/img1.mp4"));
+
+    {
+        msg += `°\n\n__${cp}\n\n✨🌺[𝐎𝐖𝐍𝐄𝐑:☞𝐒𝐎𝐇𝐀𝐆 𝐊𝐇𝐀𝐍 (✷‿✷)☜!🍂`
+
+    }
+
+    return api.sendMessage({
+        body: msg,
+        attachment: allimage
+    }, event.threadID, event.messageID);
+      }
